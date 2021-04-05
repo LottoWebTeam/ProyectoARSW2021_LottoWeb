b1b136698336f28af71904a134a053d0e33c4b16
Guillermo Castro <guillermo.castro@mail.escuelaing.edu.co>
2021-04-04 21:16:27 -0500

    config

---
+++ src/main/java/edu/eci/arsw/lottoweb/configuracion/LottoWebSocket.java
@@ -0,0 +1,27 @@
+package edu.eci.arsw.lottoweb.configuracion;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class LottoWebSocket implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/LottoWebSocket").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/LottoWebSocketViaje").setAllowedOrigins("*").withSockJS();
+    }
+
+
+
+}
