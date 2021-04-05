686b17a569ed167d654e09d77798d477468d9bcc
Guillermo Castro <guillermo.castro@mail.escuelaing.edu.co>
2021-04-05 01:55:42 -0500

    back prueba

--- src/main/java/edu/eci/arsw/lottoweb/configuracion/BaseDeDatos.java
+++ src/main/java/edu/eci/arsw/lottoweb/configuracion/BaseDeDatos.java
@@ -3,10 +3,12 @@ package edu.eci.arsw.lottoweb.configuracion;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 
+@Configuration
 public class BaseDeDatos {
 
     public static final String PRIMARY_DATASOURCE = "PrincipalDS";
